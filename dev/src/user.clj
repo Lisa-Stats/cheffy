0f697473bf15a7b1e3dca5e99b404c90a8820762
Lisa-Stats <lisa.stathopulos@gmail.com>
2020-12-22 09:47:23 -0500

    Initial commit
    Worked with Drew to set up learn reitit pro.

---
+++ dev/src/user.clj
@@ -0,0 +1,25 @@
+(ns user
+  (:require
+   [cheffy.server]
+   [integrant.core :as ig]
+   [integrant.repl :as ig-repl]
+   [integrant.repl.state :as state]
+   ))
+
+(ig-repl/set-prep!
+ (fn [] (-> "resources/config.edn"
+            slurp
+            ig/read-string)))
+
+(def go ig-repl/go)
+(def halt ig-repl/halt)
+(def reset ig-repl/reset)
+(def reset-all ig-repl/reset-all)
+
+(defn app [] (-> state/system :cheffy/app))
+(defn db [] (-> state/system :db/postgres))
+
+(comment
+  (go)
+  (halt)
+  (reset))
