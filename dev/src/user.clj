35f92b1836c767a2b94ca7aa1201a53f27ed0321
Lisa-Stats <lisa.stathopulos@gmail.com>
2021-01-07 18:07:52 -0500

    Add two different functions to return recipe by the id

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -30,6 +30,21 @@
    (with-open [conn (jdbc/get-connection db)]
      {:public (sql/find-by-keys conn :recipe {:public true})
       :drafts (sql/find-by-keys conn :recipe {:public false :uid "auth0|5ef440986e8fbb001355fd9c"})}))
+
+  (with-open [conn (jdbc/get-connection db)] ;;returns map within a vector
+    (let [recipe-id "a3dde84c-4a33-45aa-b0f3-4bf9ac997680"
+          [recipe] (sql/find-by-keys conn :recipe {:recipe_id recipe-id})
+          steps (sql/find-by-keys conn :step {:recipe_id recipe-id})
+          ingredients (sql/find-by-keys conn :ingredient {:recipe_id recipe-id})]
+      (when (seq recipe)
+        (assoc recipe
+               :recipe/steps steps
+               :recipe/ingredients ingredients))))
+
+  (with-open [conn (jdbc/get-connection db)] ;;returns only a map
+    (let [recipe-id "a3dde84c-4a33-45aa-b0f3-4bf9ac997680"]
+      (sql/get-by-id conn :recipe recipe-id :recipe_id {} )))
+
   (go)
   (halt)
   (reset))
