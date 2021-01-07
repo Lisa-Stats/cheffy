2b50f0ed171d4681bb7fe869c7e2cd014f73397c
Lisa-Stats <lisa.stathopulos@gmail.com>
2021-01-07 17:58:35 -0500

    Add handler to retrieve recipe by recipe id

--- src/cheffy/recipe/handlers.clj
+++ src/cheffy/recipe/handlers.clj
@@ -8,3 +8,14 @@
     (let [uid "auth0|5ef440986e8fbb001355fd9c"
           recipes (recipe-db/find-all-recipes db uid)]
       (rr/response recipes))))
+
+(defn retrieve-recipe
+  [db]
+  (fn [_request]
+    (let [recipe-id "a3dde84c-4a33-45aa-b0f3-4bf9ac997680"
+          recipe (recipe-db/find-recipe-by-id db recipe-id)]
+      (if recipe
+        (rr/response recipe)
+        (rr/not-found {:type "recipe-not-found"
+                       :message "Recipe not found"
+                       :data (str "recipe-id " recipe-id)})))))
