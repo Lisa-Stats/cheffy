8db0804f01ac37722144c770a5ffe750b954869d
Lisa-Stats <lisa.stathopulos@gmail.com>
2021-01-07 18:00:39 -0500

    Add new route to find recipe by id + move swagger to recipes path

--- src/cheffy/recipe/routes.clj
+++ src/cheffy/recipe/routes.clj
@@ -4,6 +4,10 @@
 (defn routes
   [config]
   (let [db (:jdbc-url config)]
-    ["/recipes" {:swagger {:tags ["recipes"]}
-                 :get {:handler (recipe/list-all-recipes db)
-                       :summary "List all recipes"}}]))
+    ["/recipes" {:swagger {:tags ["recipes"]}}
+     [""
+      {:get {:handler (recipe/list-all-recipes db)
+             :summary "List all recipes"}}]
+     ["/:recipe-id"
+      {:get (recipe/retrieve-recipe db)
+       :summary "Retrieve recipe"}]]))
