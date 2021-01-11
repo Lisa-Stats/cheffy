d9005c026fa151718c5eb4a74d97ae3b828225a2
Lisa-Stats <lisa.stathopulos@gmail.com>
2021-01-11 12:37:22 -0500

    Add :post to /recipes route

--- src/cheffy/recipe/routes.clj
+++ src/cheffy/recipe/routes.clj
@@ -9,7 +9,13 @@
      [""
       {:get {:handler (recipe/list-all-recipes db)
              :responses {200 {:body responses/recipes}}
-             :summary "List all recipes"}}]
+             :summary "List all recipes"}
+       :post {:handler (recipe/create-recipe! db)
+              :parameters {:body {:name string?
+                                  :prep-time number?
+                                  :img string?}}
+              :responses {201 {:body {:recipe-id string?}}}
+              :summary "Create recipe"}}]
      ["/:recipe-id"
       {:get {:handler (recipe/retrieve-recipe db)
              :parameters {:path {:recipe-id string?}}
